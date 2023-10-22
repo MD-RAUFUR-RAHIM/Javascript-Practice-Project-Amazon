bc09928d4e6f54cf9ebe8bb60ced4c410aff04b5
MD RAUFUR RAHIM <raufurrahim2000@gmail.com>
2023-10-22 11:43:22 +0600

    Shopping Cost  Working

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,7 @@ import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 let cartSummaryHTML = '';
 let paymentSummaryHTML = '';
-
+let matchingProduct;
 if(cart.length === 0)
 {
   cartSummaryHTML = `
@@ -50,7 +50,7 @@ if(cart.length === 0)
 else{
   cart.forEach((cartItem)=>{
     const productId= cartItem.productId;
-    let matchingProduct;
+    
     products.forEach((product) => {
       if(product.id === productId){
         matchingProduct = product;
@@ -94,7 +94,8 @@ else{
           <div class="delivery-option">
             <input type="radio" checked
               class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
+              name="delivery-option-${matchingProduct.id}"
+              value="Free Shipping">
             <div>
               <div class="delivery-option-date">
                 Tuesday, June 21
@@ -107,7 +108,8 @@ else{
           <div class="delivery-option">
             <input type="radio"
               class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
+              name="delivery-option-${matchingProduct.id}"
+              value="$4.99">
             <div>
               <div class="delivery-option-date">
                 Wednesday, June 15
@@ -120,7 +122,8 @@ else{
           <div class="delivery-option">
             <input type="radio"
               class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
+              name="delivery-option-${matchingProduct.id}"
+              value="$9.99">
             <div>
               <div class="delivery-option-date">
                 Monday, June 13
@@ -146,7 +149,7 @@ else{
   
           <div class="payment-summary-row">
             <div>Shipping &amp; handling:</div>
-            <div class="payment-summary-money">$4.99</div>
+            <div class="payment-summary-money js-payment-summary-money-button">$40</div>
           </div>
   
           <div class="payment-summary-row subtotal-row">
@@ -171,128 +174,7 @@ else{
   
   });
 }
-// cart.forEach((cartItem)=>{
-//   const productId= cartItem.productId;
-//   let matchingProduct;
-//   products.forEach((product) => {
-//     if(product.id === productId){
-//       matchingProduct = product;
-//     }
-//   });
-//   cartSummaryHTML += `
-//   <div class="cart-item-container
-//     js-cart-item-container-${matchingProduct.id}">
-//     <div class="delivery-date">
-//       Delivery date: Tuesday, June 21
-//     </div>
 
-//     <div class="cart-item-details-grid">
-//       <img class="product-image"
-//         src="${matchingProduct.image}">
-
-//       <div class="cart-item-details">
-//         <div class="product-name">
-//           ${matchingProduct.name}
-//         </div>
-//         <div class="product-price">
-//           $${formatCurrency(matchingProduct.priceCents)}
-//         </div>
-//         <div class="product-quantity">
-//           <span>
-//             Quantity: <span class="quantity-label js-quantity-label">${cartItem.quantity}</span>
-//           </span>
-//           <span class="update-quantity-link link-primary">
-//             Update
-//           </span>
-//           <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
-//             Delete
-//           </span>
-//         </div>
-//       </div>
-
-//       <div class="delivery-options">
-//         <div class="delivery-options-title">
-//           Choose a delivery option:
-//         </div>
-//         <div class="delivery-option">
-//           <input type="radio" checked
-//             class="delivery-option-input"
-//             name="delivery-option-${matchingProduct.id}">
-//           <div>
-//             <div class="delivery-option-date">
-//               Tuesday, June 21
-//             </div>
-//             <div class="delivery-option-price">
-//               FREE Shipping
-//             </div>
-//           </div>
-//         </div>
-//         <div class="delivery-option">
-//           <input type="radio"
-//             class="delivery-option-input"
-//             name="delivery-option-${matchingProduct.id}">
-//           <div>
-//             <div class="delivery-option-date">
-//               Wednesday, June 15
-//             </div>
-//             <div class="delivery-option-price">
-//               $4.99 - Shipping
-//             </div>
-//           </div>
-//         </div>
-//         <div class="delivery-option">
-//           <input type="radio"
-//             class="delivery-option-input"
-//             name="delivery-option-${matchingProduct.id}">
-//           <div>
-//             <div class="delivery-option-date">
-//               Monday, June 13
-//             </div>
-//             <div class="delivery-option-price">
-//               $9.99 - Shipping
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// `;
-// paymentSummaryHTML = `<div class="payment-summary js-payment-summary">
-//         <div class="payment-summary-title">
-//           Order Summary
-//         </div>
-
-//         <div class="payment-summary-row">
-//           <div class="js-total-quantity">Items: ${totalQuantity()}</div>
-//           <div class="payment-summary-money js-payment-summary-money"></div>
-//         </div>
-
-//         <div class="payment-summary-row">
-//           <div>Shipping &amp; handling:</div>
-//           <div class="payment-summary-money">$4.99</div>
-//         </div>
-
-//         <div class="payment-summary-row subtotal-row">
-//           <div>Total before tax:</div>
-//           <div class="payment-summary-money">$47.74</div>
-//         </div>
-
-//         <div class="payment-summary-row">
-//           <div>Estimated tax (10%):</div>
-//           <div class="payment-summary-money">$4.77</div>
-//         </div>
-
-//         <div class="payment-summary-row total-row">
-//           <div>Order total:</div>
-//           <div class="payment-summary-money">$52.51</div>
-//         </div>
-
-//         <button class="place-order-button button-primary">
-//           Place your order
-//         </button>
-//         </div>`;
-
-// });
 
 document.querySelector('.js-order-summary').innerHTML= cartSummaryHTML;
 document.querySelector('.js-payment-summary').innerHTML =paymentSummaryHTML;
@@ -329,5 +211,20 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   if(getElement){
     getElement.textContent = `Total Cost: $${totalCost(cart, products)}`;
   }
+
   
+const deliveryOptionInputs = document.querySelectorAll(`input[name^="delivery-option-"]`);
+
+// Add change event listeners to each input
+deliveryOptionInputs.forEach((input) => {
+  input.addEventListener('change', () => {
+    const selectedValue = input.value;
+    const paymentSummaryMoney = document.querySelector('.js-payment-summary-money-button');
+    if (paymentSummaryMoney) {
+      paymentSummaryMoney.innerHTML = selectedValue;
+    }
+  });
+});
+
+
 
